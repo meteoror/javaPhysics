60e69b24124457738d573a01e22ec71e55edac95
meteoror <79717779+meteoror@users.noreply.github.com>
2023-10-17 09:03:28 -0400

    Update main.java
    error

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main {
     //all the code in here wasnt tested so uh. use w caution
+    //yes this runs errorss. error: <identifier> expected ones
     JFrame window = new JFrame("Game Name");
     window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     window.getContentPane().add(emptyLabel, BorderLayout.CENTER);
