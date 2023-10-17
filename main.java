744f48fe0d403ae68c8814add5f18f6defab6e28
meteoror <79717779+meteoror@users.noreply.github.com>
2023-10-17 07:57:20 -0400

    Update main.java
    Added main window operations

--- main.java
+++ main.java
@@ -1,3 +1,8 @@
 public class main {
-    
+    //all the code in here wasnt tested so uh. use w caution
+    JFrame window = new JFrame("Game Name");
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+    window.pack();
+    window.setVisible(true);
 }
