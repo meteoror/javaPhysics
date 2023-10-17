40ad943b52daf8121cbbce33c33388ec67bdb23d
meteoror <79717779+meteoror@users.noreply.github.com>
2023-10-17 09:19:49 -0400

    Update main.java
    removed ugly comments

--- main.java
+++ main.java
@@ -2,17 +2,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a JFrame and configure it
         JFrame window = new JFrame("Game Name");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Create a JLabel (emptyLabel) or any other component you want to add
         JLabel emptyLabel = new JLabel("Hello, World!");
-
-        // Add the component to the JFrame's content pane
         window.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-
-        // Pack the window and make it visible
         window.pack();
         window.setVisible(true);
     }
