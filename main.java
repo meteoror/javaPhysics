5c715e6eb88c003f777e5caabc6e9439dd4cbdc4
meteoror <79717779+meteoror@users.noreply.github.com>
2023-10-17 09:09:47 -0400

    Update main.java
    fixed errors

--- main.java
+++ main.java
@@ -1,9 +1,19 @@
-public class main {
-    //all the code in here wasnt tested so uh. use w caution
-    //yes this runs errorss. error: <identifier> expected ones
-    JFrame window = new JFrame("Game Name");
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.getContentPane().add(emptyLabel, BorderLayout.CENTER);
-    window.pack();
-    window.setVisible(true);
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a JFrame and configure it
+        JFrame window = new JFrame("Game Name");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a JLabel (emptyLabel) or any other component you want to add
+        JLabel emptyLabel = new JLabel("Hello, World!");
+
+        // Add the component to the JFrame's content pane
+        window.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+        // Pack the window and make it visible
+        window.pack();
+        window.setVisible(true);
+    }
 }
